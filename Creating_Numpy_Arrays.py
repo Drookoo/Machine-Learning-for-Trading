8b064e0390b54e6e42a79e8d7aabf10e01140492
Drew Ku <andrewku123@gmail.com>
2017-10-25 23:05:37 -0400

    initial commit

---
+++ Creating_Numpy_Arrays.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+
+def test_run():
+    # print(np.array([(2, 3, 4), (5, 6, 7)]))
+    print(np.empty(5))
+    print(np.empty((5, 4, 3)))
+
+if __name__ == '__main__':
+    test_run()
